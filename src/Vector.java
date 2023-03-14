4cfed3d3f4ea4efe0260103fe9f7da698125dcca
iur1nott <120127934+iur1nott@users.noreply.github.com>
2023-03-14 17:36:52 -0300

    Update Vector.java

--- src/Vector.java
+++ src/Vector.java
@@ -1,18 +1,30 @@
 public class Vector {
-    private int dimensao;
-    private int[] elementos;
-
     public Vector(int dimensao, int[] elementos) {
         this.dimensao = dimensao;
         this.elementos = elementos;
     }
+    private int dimensao;
+    private int[] elementos;
+
 
     public int getElementos(int i) {
         return elementos[i];
     }
 
+
     public void set(int i, int valor) {
         this.elementos[i] = valor;
     }
-}
 
+    public String paraString() {
+        String n = "";
+
+        for(int i= 0; i<elementos.length;i++){
+            n+= elementos[i];
+            if (!(i ==elementos.length-1)) {
+                n+=" ";
+            }
+        }
+        return n;
+    }
+}
